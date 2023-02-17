2e1ed59c23afc028215926ea4588a8cd6f58a51f
Ana <al.manrod87@gmail.com>
2023-02-16 18:05:10 -0600

    Coplementando uso de Datepicker Material

--- datepicker_material/src/app/app.component.ts
+++ datepicker_material/src/app/app.component.ts
@@ -9,11 +9,19 @@ import { FormControl, FormGroup, ValidationErrors, Validators } from '@angular/f
   styleUrls: ['./app.component.css'],
  
 })
-
 export class AppComponent {
+  cp = new FormControl('');
 
+  public minDate;
+  public maxDate;
   constructor() {
 
+    let dd = String(new Date().getDate()).padStart(2,'0');
+    let mm = String(new Date().getMonth()).padStart(2,'0');
+    let yyyy = new Date().getFullYear();
+// En minDate se setea el año actual,mes actual y del día actual -(5 dias)
+    this.minDate = new Date(yyyy,parseInt(mm)-1,parseInt(dd)-5);
+    this.maxDate = new Date(yyyy,parseInt(mm)-1,parseInt(dd));
  
   }
 
@@ -29,4 +37,22 @@ export class AppComponent {
   });
 
 
+/* public tmp:any = '';
+
+  test(e:any){
+    let cps = (this.cp.value)?this.cp.value:'';
+    let size = cps.length;
+   
+    if (size === 5){
+      if (this.tmp === cps){
+        console.log("LANZA NOOOO")
+      } else {
+        this.tmp = this.cp.value;
+        console.log("LANZA WS")
+      }
+    } else {
+      console.log("nada")
+    }
+    
+  } */
 }
